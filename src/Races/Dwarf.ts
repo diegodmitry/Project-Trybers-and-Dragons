b2c9778bbeb6815085ed67241c79b8c29e4dc0ec
Diego Dmitry <diegodmitry@hotmail.com>
2022-06-27 16:47:55 -0300

    fix: fixed lint error.

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -1,12 +1,19 @@
 import Race from './Race';
 
 class Dwarf extends Race {
-  private _maxLifePoints: number = 80;
+  private _maxLifePoints: number;
   private static _instancesCreated = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this._maxLifePoints = 80;
+  }
   
   get maxLifePoints(): number {
     return this._maxLifePoints;
   }
+
   static createdRacesInstances(): number {
     this._instancesCreated += 1;
     return this._instancesCreated;
