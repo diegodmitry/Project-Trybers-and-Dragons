9c512954aeb4725e242a204506005e5ad93daacf
Diego Dmitry <diegodmitry@hotmail.com>
2022-06-27 17:42:38 -0300

    fix: fixed lint error.

--- src/Races/Orc.ts
+++ src/Races/Orc.ts
@@ -1,14 +1,20 @@
 import Race from './Race';
 
 class Orc extends Race {
-  private _maxLifePoints: number = 74;
+  private _maxLifePoints: number;
   private static _instancesCreated = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 74;
+  }
   
   get maxLifePoints(): number {
     return this._maxLifePoints;
   }
+
   static createdRacesInstances(): number {
-  this._instancesCreated += 1;
+    this._instancesCreated += 1;
     return this._instancesCreated;
   }
 }
