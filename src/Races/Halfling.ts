a4b16e6c5c82adcdc34162c4014174dbaa7e2ff7
Diego Dmitry <diegodmitry@hotmail.com>
2022-06-27 17:44:19 -0300

    fix: fixed lint error.

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -1,12 +1,18 @@
 import Race from './Race';
 
 class Halfling extends Race {
-  private _maxLifePoints: number = 60;
+  private _maxLifePoints: number;
   private static _instancesCreated = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 60;
+  }
   
   get maxLifePoints(): number {
     return this._maxLifePoints;
   }
+
   static createdRacesInstances(): number {
     this._instancesCreated += 1;
     return this._instancesCreated;
