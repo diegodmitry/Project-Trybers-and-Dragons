e1febfc9018a4472e5969ec86e3c5eb745058be0
Diego Dmitry <diegodmitry@hotmail.com>
2022-06-27 17:40:22 -0300

    fix: fixed lint error.

--- src/Races/Elf.ts
+++ src/Races/Elf.ts
@@ -1,12 +1,18 @@
 import Race from './Race';
 
 class Elf extends Race {
-  private _maxLifePoints: number = 99;
+  private _maxLifePoints: number;
   private static _instancesCreated = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 99;
+  }
   
   get maxLifePoints(): number {
     return this._maxLifePoints;
   }
+
   static createdRacesInstances(): number {
     this._instancesCreated += 1;
     return this._instancesCreated;
